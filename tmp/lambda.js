3ff8025126a31a5d1c332997d38be94059d3eaee
Mayank Sharma <mayank.sharma.uk@gmail.com>
2021-06-02 10:29:40 +0100

    Remove files

--- tmp/lambda.js
+++
@@ -1,7 +0,0 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const main = require('./main');
-const server = awsServerlessExpress.createServer(main);
-exports.handler = (event, context) => {
-    console.log("EVENT", JSON.stringify(event));
-    awsServerlessExpress.proxy(server, event, context);
-};
